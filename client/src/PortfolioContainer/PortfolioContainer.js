e396dc7b7492e701fcd2c1ddd70505f967c74323
Alden Vidal <aldenvidal25@gmail.com>
2023-01-20 16:43:45 +0800

    contact corrections

--- client/src/PortfolioContainer/PortfolioContainer.js
+++ client/src/PortfolioContainer/PortfolioContainer.js
@@ -7,14 +7,14 @@ export default function PortfolioContainer() {
             TOTAL_SCREENS.map((screen) => (
                 (screen.component) ? <screen.component screenName={screen.screen_name} key={screen.screen_name}
                     id={screen.screen_name} /> : <div key={screen.screen_name}></div>
-      ``))
+            ))
         )
 
-}
+    }
 
-return (
-    <div className='portfolio-container'>
-        {mapAllScreens()}
-    </div>
-)
+    return (
+        <div className='portfolio-container'>
+            {mapAllScreens()}
+        </div>
+    )
 }
